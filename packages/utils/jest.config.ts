7ec1f46dc532900b96ff25253d165e627748d85b
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2023-07-14 22:55:43 +0700

    number utils

---
+++ packages/utils/jest.config.ts
@@ -0,0 +1,9 @@
+const config = {
+  verbose: true,
+  testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*.(ts)',
+  ],
+}
+
+export default config
---
+++ packages/utils/src/libs/number/__tests__/index.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from '@jest/globals';
+import { formatCurrency, generateRandomAlphanumeric } from '../number'
+
+describe('sum module', () => {
+  it('should convert the number to currency format', () => {
+    expect(formatCurrency(100)).toEqual('100')
+    expect(formatCurrency(3000)).toEqual('3,000')
+    expect(formatCurrency(23000)).toEqual('23,000')
+    expect(formatCurrency(113000)).toEqual('113,000')
+    expect(formatCurrency(1343000)).toEqual('1,340,000')
+  })
+
+  it('should generate the random alphanumeric up to 5 digits', () => {
+    const pattern = [ expect.stringMatching(/^[1I0O]/) ]
+
+    expect(generateRandomAlphanumeric().split('')).toEqual(
+      expect.not.arrayContaining(pattern)
+    )
+
+    expect(generateRandomAlphanumeric().split('')).toEqual(
+      expect.not.arrayContaining(pattern)
+    )
+
+    expect(generateRandomAlphanumeric().split('')).toEqual(
+      expect.not.arrayContaining(pattern)
+    )
+  })
+})
