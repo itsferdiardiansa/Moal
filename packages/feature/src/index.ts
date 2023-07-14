92bdf65dfbfee46f9319612ae3a4571ca91d0e60
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2023-07-14 22:57:09 +0700

    number utils

--- packages/feature/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './libs'
-// import { addSum } from '@moal/utils'
-
-// console.log('utils', addSum)
--- packages/feature/src/libs/number.ts
+++
@@ -1,3 +0,0 @@
-import { addSum } from '@moal/utils'
-
-console.log('ADD SUM', addSum(2, 2))
